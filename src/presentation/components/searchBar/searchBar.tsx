e26b78aba7d92155f716fdac233f8fe5c31d734b
Elymne <sacha.djurdjevic@gmail.com>
2024-03-18 19:57:17 +0100

    feat : sarchbar text callback added

--- src/presentation/components/searchBar/searchBar.tsx
+++ src/presentation/components/searchBar/searchBar.tsx
@@ -1,26 +1,47 @@
+import { ChangeEvent, useRef } from "react"
 import styles from "./searchBar.module.css"
 import Image from "next/image"
 
 type OnSearch = (keyWords: string, city: string) => void
 
 const SearchBar = ({ onSearch }: { onSearch: OnSearch }): JSX.Element => {
-    let keyWords: string = ""
-    let city: string = ""
+    const keywordsInput = useRef<string>("")
+    const cityInput = useRef<string>("")
 
     function onClick(): void {
-        onSearch(keyWords, city)
+        onSearch(keywordsInput.current, cityInput.current)
     }
 
-    function onKeyDown(keyBoardEvent: React.KeyboardEvent<HTMLButtonElement>): void {
+    function onKeyDown(keyBoardEvent: React.KeyboardEvent<HTMLButtonElement | HTMLInputElement>): void {
         if (keyBoardEvent.code === "Enter") {
-            onSearch(keyWords, city)
+            onSearch(keywordsInput.current, cityInput.current)
         }
+    }
+
+    function onKeywordInputChange(event: ChangeEvent<HTMLInputElement>) {
+        keywordsInput.current = event.target.value
+    }
+
+    function onCityInputChange(event: ChangeEvent<HTMLInputElement>) {
+        cityInput.current = event.target.value
     }
 
     return (
         <div id={styles.main}>
-            <input id={styles.search_bar_domain_input} type="text" placeholder="Recherche par mots-clés" />
-            <input id={styles.search_bar_city_input} type="text" placeholder="Paris, Lion, Nantes..." />
+            <input
+                id={styles.search_bar_domain_input}
+                type="text"
+                placeholder="Recherche par mots-clés"
+                onKeyDown={onKeyDown}
+                onChange={onKeywordInputChange}
+            />
+            <input
+                id={styles.search_bar_city_input}
+                type="text"
+                placeholder="Paris, Lion, Nantes..."
+                onKeyDown={onKeyDown}
+                onChange={onCityInputChange}
+            />
             <button onClick={onClick} onKeyDown={onKeyDown}>
                 <Image src="svg/magnifying_glass.svg" height={30} width={30} alt="Icone de loupe" />
             </button>

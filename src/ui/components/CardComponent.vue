97a4110260930b475ed28bd0f60b187a432ec021
Elymne <sacha.djurdjevic@gmail.com>
2025-02-12 10:29:11 +0100

    inprog : card 1

---
+++ src/ui/components/CardComponent.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+defineProps(['title', 'company', 'companyLogo', 'zone', 'JobTitle', 'date'])
+</script>
+
+<template>
+  <article>
+    <a>
+      <img class="top-image" src="./../assets/images/background_top.jpg" />
+      <div>
+        <h2>{{ title }}</h2>
+        <p class="company">{{ company }}</p>
+        <p class="zone">{{ zone }}</p>
+        <p class="date">{{ date }}</p>
+      </div>
+    </a>
+  </article>
+</template>
+
+<style scoped>
+article {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
+
+a {
+  border: 1px solid #e9e9e9;
+  border-radius: 10px;
+  background-color: #ffffff;
+
+  transition: transform 0.2s ease-in-out;
+}
+
+a:hover {
+  transform: scale(0.94);
+  background: #ffffff !important;
+  border: 1px solid #cacaca;
+}
+
+img.top-image {
+  width: 100%;
+  height: 100px;
+  border-radius: 10px 10px 0px 0px;
+  object-fit: cover;
+}
+
+div {
+  padding: 0 10px 10px 10px;
+}
+
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+p.company {
+  font-style: normal;
+}
+
+p.zone {
+  font-style: italic;
+  font-family: 'quicksand-light';
+}
+
+p.date {
+  margin-top: 20px;
+  font-family: 'quicksand-light';
+}
+</style>
--- src/ui/components/QuickLookBlock.vue
+++ src/ui/components/QuickLookBlock.vue
@@ -1,13 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Card from './CardComponent.vue'
+</script>
 
 <template>
   <section id="QuickLookBlock" class="responsive">
-    <h1>Bonjour, test</h1>
+    <h1>C'est quoi les offres aujourd'hui ?</h1>
+    <Card
+      title="Super, c'est un titre"
+      company="Une société qui ne vous veut que du mal"
+      zone="Quelque part ici"
+      date="feb, 23 2020"
+    />
   </section>
 </template>
 
 <style scoped>
 section {
-  margin-top: 100px;
+  margin-top: 40px;
+}
+
+h1 {
+  margin-bottom: 40px;
 }
 </style>

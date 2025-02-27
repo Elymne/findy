cf99b30dee49fe4ba23296d025773393cd53380b
Elymne <sacha.djurdjevic@gmail.com>
2025-02-27 08:45:30 +0100

    rework : footer

--- src/ui/components/CustomFooter.vue
+++ src/ui/components/CustomFooter.vue
@@ -3,13 +3,12 @@
 <template>
   <footer>
     <section>
-      <h1>Informations</h1>
+      <h1 id="info">Informations</h1>
       <a href="#">Politique de confidentialité</a>
       <a href="#">Mentions légales</a>
       <a href="#">CGU</a>
-    </section>
-    <section>
-      <h1>A propos</h1>
+
+      <h1 id="about">A propos</h1>
       <a href="#">Qui sommes-nous ?</a>
       <a href="#">Le concept</a>
       <a href="#">Offres d'alternance</a>
@@ -19,18 +18,25 @@
 
 <style scoped>
 footer {
-  background-color: rgb(218, 218, 218);
+  background-color: rgb(48, 44, 44);
   border-radius: 20px;
 }
 
 section {
-  margin-left: 20px;
+  padding: 40px;
+}
+
+#about {
   margin-top: 20px;
-  margin-bottom: 40px;
+}
+
+h1 {
+  color: white;
 }
 
 a {
   margin-right: 20px;
+  color: white;
 }
 
 @media (min-width: 1280px) {
@@ -43,18 +49,21 @@ a {
 @media (max-width: 1280px) and (min-width: 780px) {
   footer {
     margin: auto 40px;
+    margin-bottom: 40px;
   }
 }
 
 @media (max-width: 780px) and (min-width: 480px) {
   footer {
     margin: auto 0;
+    border-radius: 0px;
   }
 }
 
 @media (max-width: 480px) {
   footer {
     margin: auto 0;
+    border-radius: 0px;
   }
 }
 </style>
